ef1f3a4e8c917304cc6840a276bcaa1a6a6790f3
Abiola Adebanjo <abiola.adebanjo@interswitchgroup.com>
2016-10-25 18:46:44 +0100

    Refactoring

--- src/main/java/com/interswitch/techquest/payment/gateway/sample/PaymentGateway.java
+++ src/main/java/com/interswitch/techquest/payment/gateway/sample/PaymentGateway.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.interswitch.techquest.payment.gateway.sample;
 
 import com.interswitch.techquest.secure.utils.InterswitchAuth;
@@ -20,17 +15,17 @@ import org.json.JSONObject;
  * @author Abiola.Adebanjo
  */
 public class PaymentGateway {
-    
-    public static String generateRef() {
+
+    private static String generateRef() {
         UUID nonce = UUID.randomUUID();
         String transRef = "ISW|API|JAM|" + nonce.toString().replaceAll("-", "");
         return transRef;
     }
-    
-    public static HashMap<String, String> doREST(String clientAccessToken, String resourceUrl, String httpMethod, String request) throws Exception {
+
+    private static HashMap<String, String> doREST(String clientAccessToken, String resourceUrl, String httpMethod, String request) throws Exception {
         HashMap<String, String> response = new HashMap<String, String>();
         HashMap<String, String> securityHeaders = InterswitchAuth.getBearerSecurityHeaders(Constants.CLIENT_ID, Constants.CLIENT_SECRET, clientAccessToken, resourceUrl, httpMethod, null);
-        
+
         URL obj = new URL(resourceUrl);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod(httpMethod);
@@ -40,7 +35,7 @@ public class PaymentGateway {
         con.setRequestProperty("Nonce", securityHeaders.get("NONCE"));
         con.setRequestProperty("SignatureMethod", securityHeaders.get("SIGNATURE_METHOD"));
         con.setRequestProperty("Signature", securityHeaders.get("SIGNATURE"));
-        
+
         if (request != null) {
             con.setDoOutput(true);
             DataOutputStream wr = new DataOutputStream(con.getOutputStream());
@@ -48,7 +43,7 @@ public class PaymentGateway {
             wr.flush();
             wr.close();
         }
-        
+
         int responseCode = con.getResponseCode();
         System.out.println("\nSending " + httpMethod + " request to URL : " + resourceUrl);
         System.out.println("Post parameters : " + request);
@@ -61,7 +56,7 @@ public class PaymentGateway {
             in = new BufferedReader(new InputStreamReader(con.getErrorStream()));
 //            throw ex;
         }
-        
+
         String inputLine;
         StringBuffer responseBuffer = new StringBuffer();
         while ((inputLine = in.readLine()) != null) {
@@ -70,13 +65,13 @@ public class PaymentGateway {
         in.close();
         JSONObject jSONObjectx = new JSONObject(responseBuffer.toString());
         System.out.println(jSONObjectx.toString(2));
-        
+
         response.put(Constants.HTTP_CODE, String.valueOf(responseCode));
         response.put(Constants.RESPONSE_BODY, responseBuffer.toString());
-        
+
         return response;
     }
-    
+
     public static HashMap<String, String> doValidation(String clientAccessToken, String authData) throws Exception {
         String httpMethod = "POST";
         String transactionRef = generateRef(); // unique id to identify each request
@@ -84,24 +79,24 @@ public class PaymentGateway {
         JSONObject jSONObject = new JSONObject();
         jSONObject.put("authData", authData);
         jSONObject.put("transactionRef", transactionRef);
-        
+
         String request = jSONObject.toString();
-        
+
         return doREST(clientAccessToken, Constants.VALIDATION_RESOURCE_URL, httpMethod, request);
     }
-    
+
     public static HashMap<String, String> doValidationAuthOTP(String clientAccessToken, String otp, String transactionRef) throws Exception {
         String httpMethod = "POST";
-        
+
         JSONObject jSONObject = new JSONObject();
         jSONObject.put("otp", otp);
         jSONObject.put("transactionRef", transactionRef);
-        
+
         String request = jSONObject.toString();
-        
+
         return doREST(clientAccessToken, Constants.VALIDATION_AUTH_OTP_RESOURCE_URL, httpMethod, request);
     }
-    
+
     public static HashMap<String, String> doPurchase(String clientAccessToken, String authData, String amount) throws Exception {
         String httpMethod = "POST";
         String transactionRef = generateRef();
@@ -114,28 +109,28 @@ public class PaymentGateway {
         jSONObject.put("currency", currency);
         jSONObject.put("authData", authData);
         jSONObject.put("transactionRef", transactionRef);
-        
+
         String request = jSONObject.toString();
-        
+
         return doREST(clientAccessToken, Constants.PURCHASE_RESOURCE_URL, httpMethod, request);
     }
-    
+
     public static HashMap<String, String> doPurchaseAuthOTP(String clientAccessToken, String otp, String paymentId) throws Exception {
         String httpMethod = "POST";
-        
+
         JSONObject jSONObject = new JSONObject();
         jSONObject.put("otp", otp);
         jSONObject.put("paymentId", paymentId);
-        
+
         String request = jSONObject.toString();
-        
+
         return doREST(clientAccessToken, Constants.PURCHASE_AUTH_OTP_RESOURCE_URL, httpMethod, request);
     }
-    
+
     public static HashMap<String, String> doTransactionQuery(String clientAccessToken, String amount, String transactionRef) throws Exception {
         String httpMethod = "GET";
         String transactionQueryUrl = Constants.PURCHASE_RESOURCE_URL + "?amount=" + amount + "&transactionRef=" + transactionRef;
-        
+
         return doREST(clientAccessToken, transactionQueryUrl, httpMethod, null);
     }
 }
